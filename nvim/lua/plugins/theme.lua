0299a5466f439612578bdea6c398770691097339
Damon Kreft <damon.kreft@beautybay.com>
2024-08-17 22:50:34 +0100

    First stab

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,18 @@
+return {
+  { "loctvl842/monokai-pro.nvim", name = "monokai-pro", priority = 1000, opts = {
+    flavour = "mocha",
+  } },
+  {
+    "navarasu/onedark.nvim",
+    name = "onedark",
+    opts = {
+      style = "warmer",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
